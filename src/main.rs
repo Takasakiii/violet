c2414455386f63623d741df39abea755464720fc
Takasakiii <lucasmc2709@live.com>
2022-03-27 17:29:39 -0300

    adicionado: conexao a database no startup e chamando a migração

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod config;
+mod database;
 
 use std::io;
 
 use actix_web::{middleware::Logger, App, HttpServer};
 use config::Config;
+use database::Database;
 use env_logger::Env;
 
 #[tokio::main]
@@ -11,6 +13,9 @@ async fn main() -> io::Result<()> {
     env_logger::init_from_env(Env::new().default_filter_or("info"));
 
     let config = Config::get_config();
+
+    let database = Database::new(&config).await;
+    database.migrate().await;
 
     HttpServer::new(|| App::new().wrap(Logger::default()))
         .bind(("0.0.0.0", config.server_port))?
