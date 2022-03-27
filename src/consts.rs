99b0e3a1ab6c58329bda14959f73f926824f0f8b
Takasakiii <lucasmc2709@live.com>
2022-03-27 14:04:11 -0300

    restartando o projeto

--- src/consts.rs
+++
@@ -1,3 +0,0 @@
-pub mod colors {
-    pub const VIOLET:u32 =  0xEE82EE;
-}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,3 @@
-mod webserver;
-mod discordbot;
-pub mod config;
-pub mod consts;
-pub mod tokens;
-mod mysql_db;
-pub mod channels;
-
-use dotenv::dotenv;
-
-#[macro_use]
-extern crate lazy_static;
-
-pub type GenericError = Box<dyn std::error::Error + Send + Sync>;
-
-#[tokio::main]
-async fn main() {
-    dotenv()
-        .ok();
-
-    std::env::set_var("RUST_LOG", "actix_web=info");
-    env_logger::init();
-
-    mysql_db::create_database()
-        .expect("Não foi possivel criar a database.");
-
-    discordbot::start();
-    webserver::start_web_server();
+fn main() {
+    println!("Hello, world!");
 }
-
-
