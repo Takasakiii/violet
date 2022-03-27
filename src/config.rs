c98f148b28269bc192d44a038ee594a3c11f8f01
Takasakiii <lucasmc2709@live.com>
2022-03-27 17:28:51 -0300

    adicionando: configurações para a database

--- src/config.rs
+++ src/config.rs
@@ -7,11 +7,15 @@ use serde::Deserialize;
 #[derive(Deserialize)]
 pub struct Config {
     pub server_port: u16,
+    pub database_url: String,
 }
 
 impl Default for Config {
     fn default() -> Self {
-        Self { server_port: 3000 }
+        Self {
+            server_port: 3000,
+            database_url: String::from("mysql://root:root@localhost:3306/violet"),
+        }
     }
 }
 
