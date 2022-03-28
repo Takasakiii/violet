cf0d233e3236e0650254e653d79ebb6e336016c1
Takasakiii <lucasmc2709@live.com>
2022-03-27 23:27:11 -0300

    adicionado: endpoint de login

--- src/controllers/auth.rs
+++ src/controllers/auth.rs
@@ -6,9 +6,12 @@ use actix_web::{
 use serde::Deserialize;
 use validator::Validate;
 
-use crate::database::{
-    users::{self, Users, UsersErrors},
-    Database,
+use crate::{
+    database::{
+        users::{self, Users, UsersErrors, UsersGetByUsernameErrors},
+        Database,
+    },
+    jwt::Jwt,
 };
 
 #[derive(Deserialize, Validate, Debug)]
@@ -58,6 +61,65 @@ pub async fn sing_up(sing_up: Json<SingUpDto>, database: Data<Database>) -> Http
         })),
         Err(UsersErrors::Generic(e)) => {
             log::error!("Failed to create user: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+#[derive(Deserialize)]
+pub struct LoginDto {
+    username: String,
+    password: String,
+}
+
+#[post("/login")]
+pub async fn login(
+    login_dto: Json<LoginDto>,
+    database: Data<Database>,
+    jwt: Data<Jwt>,
+) -> HttpResponse {
+    let login = login_dto.into_inner();
+
+    let user = match users::get_by_username(&*database, login.username).await {
+        Ok(user) => user,
+        Err(UsersGetByUsernameErrors::NotFound) => {
+            return HttpResponse::Unauthorized().json(serde_json::json!({
+                "error": "Username or password is incorrect"
+            }));
+        }
+        Err(UsersGetByUsernameErrors::Generic(e)) => {
+            log::error!("Failed to get user: {}", e);
+            return HttpResponse::InternalServerError().finish();
+        }
+    };
+
+    match bcrypt::verify(login.password, &user.password_hash) {
+        Ok(true) => {
+            let token = jwt.create_jwt(user.username.clone());
+            let token = match token {
+                Ok(token) => {
+                    if let Err(err) =
+                        users::add_last_token(&*database, token.clone(), user.username).await
+                    {
+                        log::error!("Failed to add last token: {}", err);
+                        return HttpResponse::InternalServerError().finish();
+                    }
+
+                    token
+                }
+                Err(err) => {
+                    log::error!("Failed to create token: {}", err);
+                    return HttpResponse::InternalServerError().finish();
+                }
+            };
+
+            HttpResponse::Ok().json(serde_json::json!({ "token": token }))
+        }
+        Ok(false) => HttpResponse::Unauthorized().json(serde_json::json!({
+            "error": "Username or password is incorrect"
+        })),
+        Err(e) => {
+            log::error!("Failed to verify password: {}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
