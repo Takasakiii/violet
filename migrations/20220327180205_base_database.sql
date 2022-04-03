4451ed7695210e94a25079ae92d0ac82248e4074
Takasakiii <lucasmc2709@live.com>
2022-04-03 00:51:37 -0300

    removido: o customizador de erros

--- migrations/20220327180205_base_database.sql
+++ migrations/20220327180205_base_database.sql
@@ -23,28 +23,18 @@ create table app_tokens (
     primary key (token)
 );
 
-create table errors_level (
-    id int not null auto_increment,
-    name varchar(255) not null,
-    app_id int not null,
-    position int not null,
-    color int unsigned not null,
-    unique key errors_app_position (app_id, position),
-    unique key errors_app_name (app_id, name),
-    foreign key (app_id) references apps(id),
-    primary key (id)
-);
 
 
 create table errors (
     id int not null auto_increment,
     app_id int not null,
-    error_level_id int not null,
+    error_level varchar(20) not null,
     message text not null,
     stack_trace text,
+    token varchar(255) not null,
     created_at bigint unsigned not null,
     foreign key (app_id) references apps(id),
-    foreign key (error_level_id) references errors_level(id),
+    foreign key (token) references app_tokens(token),
     primary key (id)
-)
+);
 
